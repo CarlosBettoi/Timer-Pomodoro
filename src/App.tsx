269a8848d975cb8fdc6b53ad8e21890b1fee319f
Carlos bettoi <carlosbettoi@yahoo.com>
2023-04-03 21:23:43 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./components/Button";
+
+
+export function App() {
+  
+
+  return (
+   <>
+
+    <Button variant="primary"/>
+    <Button variant="secondary"/>
+    <Button variant="success"/>
+    <Button variant="danger"/>
+    <Button/>
+  </>
+   
+  )
+
+
+}
