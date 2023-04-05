f8541366e23f0d1de529b7a8ab49e93328af299c
Carlos bettoi <carlosbettoi@yahoo.com>
2023-04-05 10:45:47 -0300

    estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {ThemeProvider} from 'styled-components'
 import { Button } from "./components/Button";
+
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 
 export function App() {
@@ -15,6 +17,8 @@ export function App() {
     <Button variant="success"/>
     <Button variant="danger"/>
     <Button/>
+
+   <GlobalStyle/> 
  
  </ThemeProvider>
  
